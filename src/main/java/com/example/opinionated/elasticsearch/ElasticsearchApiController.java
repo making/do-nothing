858187274a9e8b39910b6682b94118278a54c0cb
Toshiaki Maki <makingx@gmail.com>
2024-07-10 18:53:24 +0900

    Add dummy `_msearch` api

--- src/main/java/com/example/opinionated/elasticsearch/ElasticsearchApiController.java
+++ src/main/java/com/example/opinionated/elasticsearch/ElasticsearchApiController.java
@@ -2,6 +2,8 @@ package com.example.opinionated.elasticsearch;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,6 +40,50 @@ public class ElasticsearchApiController {
 				  }
 				}
 				""".formatted(index);
+	}
+
+	@PostMapping(path = "/_msearch")
+	public String msearch(@RequestBody String body) {
+		System.out.println("body="+body);
+		return """
+				{
+				  "took": 46,
+				  "responses": [
+				    {
+				      "took": 45,
+				      "timed_out": false,
+				      "_shards": {
+				        "total": 1,
+				        "successful": 1,
+				        "skipped": 0,
+				        "failed": 0
+				      },
+				      "hits": {
+				        "total": {
+				          "value": 1,
+				          "relation": "eq"
+				        },
+				        "max_score": 1.0,
+				        "hits": [
+				          {
+				            "_index": "logs",
+				            "_id": "UYRpm5AB3a7xj_r-FDPT",
+				            "_score": 1.0,
+				            "_source": {
+				              "@timestamp": "2099-11-15T13:12:00",
+				              "message": "GET /search HTTP/1.1 200 1070000",
+				              "user": {
+				                "id": "kimchy"
+				              }
+				            }
+				          }
+				        ]
+				      },
+				      "status": 200
+				    }
+				  ]
+				}
+				""";
 	}
 
 }
