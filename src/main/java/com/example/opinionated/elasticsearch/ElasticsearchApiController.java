41a5991de6bdc8236da190d350f84efb35f6deac
Toshiaki Maki <makingx@gmail.com>
2024-07-10 15:15:39 +0900

    Add `/{index}/_mapping`

--- src/main/java/com/example/opinionated/elasticsearch/ElasticsearchApiController.java
+++ src/main/java/com/example/opinionated/elasticsearch/ElasticsearchApiController.java
@@ -1,44 +1,17 @@
 package com.example.opinionated.elasticsearch;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ElasticsearchApiController {
 
-	@GetMapping(path = { "/_mapping", "/*/_mapping" })
+	@GetMapping(path = "/_mapping")
 	public String mapping() {
 		return """
 				{
-				  "logs": {
-				    "mappings": {
-				      "properties": {
-				        "@timestamp": { "type": "date" },
-				        "log_id": { "type": "integer" },
-				        "timestamp": { "type": "date" },
-				        "observed_timestamp": { "type": "date" },
-				        "severity_text": { "type": "text" },
-				        "severity_number": { "type": "integer" },
-				        "service_name": { "type": "text" },
-				        "scope": { "type": "text" },
-				        "body": { "type": "text" },
-				        "trace_id": { "type": "text" },
-				        "span_id": { "type": "text" },
-				        "trace_flags": { "type": "integer" },
-				        "attributes": { "type": "object" },
-				        "resource_attributes": { "type": "object" }
-				      }
-				    }
-				  }
-				}
-				""";
-	}
-
-	@GetMapping(path = "/*/_mapping")
-	public String mappingForIndex() {
-		return """
-				{
-				  "mappings": {
+				  "_doc": {
 				    "properties": {
 				      "@timestamp": { "type": "date" },
 				      "log_id": { "type": "integer" },
@@ -58,6 +31,34 @@ public class ElasticsearchApiController {
 				  }
 				}
 				""";
+	}
+
+	@GetMapping(path = "/{index}/_mapping")
+	public String mappingForIndex(@PathVariable String index) {
+		return """
+				{
+				  "%s": {
+				    "_doc": {
+				      "properties": {
+				        "@timestamp": { "type": "date" },
+				        "log_id": { "type": "integer" },
+				        "timestamp": { "type": "date" },
+				        "observed_timestamp": { "type": "date" },
+				        "severity_text": { "type": "text" },
+				        "severity_number": { "type": "integer" },
+				        "service_name": { "type": "text" },
+				        "scope": { "type": "text" },
+				        "body": { "type": "text" },
+				        "trace_id": { "type": "text" },
+				        "span_id": { "type": "text" },
+				        "trace_flags": { "type": "integer" },
+				        "attributes": { "type": "object" },
+				        "resource_attributes": { "type": "object" }
+				      }
+				    }
+				  }
+				}
+				""".formatted(index);
 	}
 
 }
